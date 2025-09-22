84ef7c415e734cc48b19e4d6ab7391bfdf019b20
ssillva1413 <ssillva1413@gmail.com>
2025-09-22 16:44:24 -0300

    commit

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,28 +1,20 @@
 import React from 'react';
-
-import styles from'./Header.module.css';
-import {Link} from 'react-router-dom'
+import styles from './Header.module.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
-    
   return (
     <header className={styles.header}>
       <Link to='/'>
-       <img src="logo.jpg" alt="Logo" className={styles.logo} />
+        <img src="logo.jpg" alt="Logo" className={styles.logo} />
       </Link>
       <nav>
-
-       
-        <Link to ='/'>HOME</Link>
-        <Link to ='/sobre'>SOBRE</Link>
-        <Link to ='/especialidades'>ESPECIALIDADES</Link>
-        <Link to = '/cartão'>CLINICA</Link>
-        <Link to ='/noticias'>NOTICIAS</Link>
-        <Link to ='/projetos'>PROJETOS</Link>
-       
-        
-        
-
+        <Link to='/'>HOME</Link>
+        <Link to='/sobre'>SOBRE</Link>
+        <Link to='/especialidades'>ESPECIALIDADES</Link>
+        <Link to='/cartão'>CLINICA</Link>
+        <Link to='/noticias'>NOTICIAS</Link>
+        <Link to='/projetos'>PROJETOS</Link>
       </nav>
     </header>
   );
