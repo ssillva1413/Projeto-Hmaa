f070566f8bdca2b1e39bc3b3a1c61f4403a1a672
ssillva1413 <ssillva1413@gmail.com>
2025-09-23 12:38:02 -0300

    novo commit

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import styles from './Header.module.css';
 import { Link } from 'react-router-dom';
-import { FaBars, FaTimes } from 'react-icons/fa'; // ícones do menu
+import { FaBars, FaTimes } from 'react-icons/fa';
 
 function Header() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -12,7 +12,8 @@ function Header() {
 
   return (
     <header className={styles.header}>
-      <Link to='/'>
+      {/* Logo */}
+      <Link to='/' onClick={() => setMenuOpen(false)}>
         <img src="logo.jpg" alt="Logo" className={styles.logo} />
       </Link>
 
@@ -26,14 +27,17 @@ function Header() {
         <Link to='/projetos'>PROJETOS</Link>
       </nav>
 
-      {/* Botão hambúrguer (aparece só no mobile) */}
+      {/* Ícone hambúrguer (mobile) */}
       <div className={styles.menuIcon} onClick={toggleMenu}>
-        {menuOpen ? <FaTimes /> : <FaBars />}
+        <FaBars />
       </div>
 
-      {/* Menu mobile */}
-      {menuOpen && (
-        <nav className={styles.mobileMenu}>
+      {/* Drawer lateral mobile */}
+      <div className={`${styles.mobileDrawer} ${menuOpen ? styles.open : ''}`}>
+        <div className={styles.drawerHeader}>
+          <FaTimes className={styles.closeIcon} onClick={toggleMenu} />
+        </div>
+        <nav className={styles.drawerMenu}>
           <Link to='/' onClick={toggleMenu}>HOME</Link>
           <Link to='/sobre' onClick={toggleMenu}>SOBRE</Link>
           <Link to='/especialidades' onClick={toggleMenu}>ESPECIALIDADES</Link>
@@ -41,7 +45,10 @@ function Header() {
           <Link to='/noticias' onClick={toggleMenu}>NOTICIAS</Link>
           <Link to='/projetos' onClick={toggleMenu}>PROJETOS</Link>
         </nav>
-      )}
+      </div>
+
+      {/* Overlay escuro atrás do menu */}
+      {menuOpen && <div className={styles.overlay} onClick={toggleMenu}></div>}
     </header>
   );
 }
